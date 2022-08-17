a6468f923822b0b8a9da6fd7328fd4b420c7d1d0
posesop <posesop@gmail.com>
2022-08-17 14:26:41 +0900

    feat (add) : get kakao token

--- pages/login/kakaoLogin/index.tsx
+++ pages/login/kakaoLogin/index.tsx
@@ -4,32 +4,39 @@ import {useRouter} from "next/router";
 
 
 export default function KakaoLogin() {
-
-    const PARAMS = new URL(`${document.location}`).searchParams;
-    const KAKAO_CODE = PARAMS.get('code');
     const router = useRouter();
-    const REST_API_KEY = "1b9c10e5284a52df35c6a15893708dd2";
-    const REDIRECT_URI =  "http://localhost:3000/login/kakaoLogin";
-    const getKakaoToken = () => {
-        fetch(`https://kauth.kakao.com/oauth/token` , {
-            method : 'POST',
-            headers : { 'Content-type' : 'application/x-www-form-urlencoded' },
-            body : `grant_type=authorization_code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&code=${KAKAO_CODE}`
-        })
-            .then( res =>  res.json() )
-            .then( data => {
-                if (data.access_token) {
-                    localStorage.setItem( 'token' , data.access_token )
-                } else {
-                    router.push("/login")
-                }
+    useEffect( () => {
+        const newLocation = document.location
+        console.log(newLocation)
+
+        const PARAMS = new URL(`${newLocation}`).searchParams;
+        const KAKAO_CODE = PARAMS.get('code');
+        console.log(KAKAO_CODE)
+        const REST_API_KEY = "1b9c10e5284a52df35c6a15893708dd2";
+        const REDIRECT_URI =  "http://localhost:3000/login/kakaoLogin";
+        const getKakaoToken = () => {
+            fetch(`https://kauth.kakao.com/oauth/token` , {
+                method : 'POST',
+                headers : { 'Content-type' : 'application/x-www-form-urlencoded' },
+                body : `grant_type=authorization_code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&code=${KAKAO_CODE}`
             })
-    }
-    useEffect( () =>  { if(!KAKAO_CODE) { getKakaoToken(); }},[])
+                .then( res =>  res.json() )
+                .then( data => {
+                    if (data.access_token) {
+                        localStorage.setItem( 'token' , data.access_token )
+                    } else {
+                        router.push("/login")
+                    }
+                })
+        }
+        if(!KAKAO_CODE) { getKakaoToken()}
+    }, [])
+
 
     return(
         <>
-            kakaotoken
+            <p>kakaotoken</p>
+
         </>
     )
 }
