24d7e375d65530cb961e2a5b48d43db5bdfc0362
posesop <posesop@gmail.com>
2022-08-17 22:23:35 +0900

    get kakao infomation

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -7,12 +7,7 @@ export default function Header() {
                 <div className="container mx-auto flex p-5 justify-center items-center">
                     <Link href="/">
                         <a className="flex title-font font-medium items-center justify-center text-gray-900 md:mb-0">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor"
-                                 strokeLinecap="round"
-                                 strokeLinejoin="round" strokeWidth="2"
-                                 className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-                                <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                            </svg>
+                            <img src="/favicon.ico" className="w-8"/>
                             <span className="ml-3 text-xl">MY-NEXT</span>
                         </a>
                     </Link>
--- pages/login/kakaoLogin/index.tsx
+++ pages/login/kakaoLogin/index.tsx
@@ -1,27 +1,41 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {useRouter} from "next/router";
+// import { db } from "../../../firebase";
+// import { collection , addDoc } from "@firebase/firestore";
 
 
 export default function KakaoLogin() {
-    const router = useRouter();
+    // const [kakaoData, setKakaoData ] = useState([]);
+    // useEffect(() => {
+    //     const q = query(collection(db, "kakaoData"));
+    //     const unsub = onSnapshot( q, (querySnapshot) => {
+    //         let kakaoArray: any[] = [];
+    //         querySnapshot.forEach((doc) => {
+    //             kakaoArray.push({...doc.data(), id: doc.id})
+    //         });
+    //         // @ts-ignore
+    //         setKakaoData(kakaoArray)
+    //     });
+    //     return (() => unsub())
+    // }, []);
 
+    // const [nickname , setNickname ] = useState("")
+    const router = useRouter();
     const REST_API_KEY = "1b9c10e5284a52df35c6a15893708dd2";
     const REDIRECT_URI =  "https://jordanlim.vercel.app/login/kakaoLogin";
+        useEffect( () => {
+            const newLocation = document.location
+            // console.log(newLocation)
+
+            const PARAMS = new URL(`${newLocation}`).searchParams;
+            const KAKAO_CODE = PARAMS.get('code');
+            // console.log("KAKAO_CODE : " , KAKAO_CODE )
+
+            getKakaoToken(KAKAO_CODE);
+            console.log("getKakaoToken...")
 
 
-    useEffect( () => {
-        const newLocation = document.location
-        // console.log(newLocation)
-
-        const PARAMS = new URL(`${newLocation}`).searchParams;
-        const KAKAO_CODE = PARAMS.get('code');
-        // console.log("KAKAO_CODE : " , KAKAO_CODE )
-        getKakaoToken(KAKAO_CODE);
-        console.log("getKakaoToken...")
-
-
-    }, [])
-
+        }, [])
 
     const getKakaoToken = (KAKAO_CODE : any) => {
         fetch(`https://kauth.kakao.com/oauth/token` , {
@@ -29,9 +43,7 @@ export default function KakaoLogin() {
             headers : { 'Content-type' : 'application/x-www-form-urlencoded' },
             body : `grant_type=authorization_code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&code=${KAKAO_CODE}`
         })
-            .then( (res) =>
-                res.json()
-            )
+            .then( (res) => res.json() )
             .then( data => {
                 if (data.access_token) {
                     localStorage.setItem( 'token' , data.access_token )
@@ -51,13 +63,41 @@ export default function KakaoLogin() {
             },
         })
             .then( res => res.json() )
-            .then( res => localStorage.setItem("이름" , res.kakao_account.profile.nickname ))
+            // .then( (res) => {
+                // setNickname(res.kakao_account.profile.nickname)
+                // setId(res.kakao_account.id)
+                // res.kakao_account.profile_image_needs_agreement === false ? setProfileImg(res.kakao_account.profile.profile_image_url) : setProfileImg("")
+                // res.kakao_account.email_needs_agreement === false ? setEmail(res.kakao_account.email.email) : setEmail("")
+                // res.kakao_account.gender_needs_agreement === false ? setGender(res.kakao_account.gender) : setGender("")
+                // res.kakao_account.birthday_needs_agreement === false ? setBirthday(`${res.kakao_account.birthday.birthyear}.${res.kakao_account.birthday.birthday} `) : setBirthday("")
+                // res.kakao_account.age_range_needs_agreement === false ? setAge(res.kakao_account.age_range) : setAge("")
+            // })
+            .then( (res) => localStorage.setItem("이름" , res.kakao_account.profile.nickname ))
             .then( () => {localStorage.setItem("login" , String(true) )} )
+            // .then( () => firebasePush )
             .then( () => { router.push("/")} )
-
     }
 
+    // const [id , setId ] = useState("")
+    // const [email , setEmail ] = useState("")
+    // const [gender , setGender ] = useState("")
+    // const [age , setAge ] = useState("")
+    // const [birthday , setBirthday ] = useState("")
+    // const [profileImg , setProfileImg ] = useState("")
 
+
+    // const firebasePush =  async () => {
+    //         console.log("id,email,gender,age,birthday,profileImg,nickname" + id,email,gender,age,birthday,profileImg,nickname)
+    //     await addDoc(collection(db,"kakaoData"), {
+    //         id,
+    //         nickname,
+    //         profileImg,
+    //         email,
+    //         gender,
+    //         age,
+    //         birthday,
+    //     });
+    // }
 
 
     return(
